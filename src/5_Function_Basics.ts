49569473eb5213d50187613b2a371c80bc25c03c
Muhammad Arsalan Manzoor <44143816+arsalan98m@users.noreply.github.com>
2021-02-02 11:13:15 +0500

    Practicing Functions basics

--- src/5_Function_Basics.ts
+++ src/5_Function_Basics.ts
@@ -19,3 +19,96 @@ const sum = (a:number,b:number, c?:number|string = 10) => {
 
 sum(5, 10);
 sum(10,10, "abc")
+
+
+/*
+===============
+Examples
+================
+*/
+
+/*
+function foo(p1:{a:string,b:number}){
+    let {a,b} = p1;
+    console.log("a=>",a);
+    console.log("b=>",b);
+}
+
+foo({a:"abc",b:10});
+*/
+
+
+// let say sometime we will not able to pass a value of error Obviously its going to be a compilation error
+// beacuse the required object is supposed to be have a two properties a and b 
+// foo({a:"A"}) -> Error
+
+// To get rid of this problem by putting b as option using ? mark
+
+/*
+function foo(p1:{a:string,b?:number}){
+    let {a,b} = p1;
+    console.log("a=>",a); // A
+    console.log("b=>",b); // undefined
+}
+
+foo({a:"A"});
+*/
+
+// In this case b will be undefined so what will be the case we dont want undefined multiple cases
+// one of the solution is at the time of desstructuring i say value of b=100 so we are able to
+// provide a default value at the time of destructuring
+
+/*
+function foo(p1:{a:string,b?:number}){
+    let {a,b=100} = p1;
+    console.log("a=>",a); 
+    console.log("b=>",b); 
+}
+
+foo({a:"A"});
+foo({a:"A",b:90});
+*/
+
+
+// Calling foo without passing any data as an object how we will do that "a" as an optional
+
+/*
+function foo(p1:{a?:string,b?:number}){
+    let {a="",b=100} = p1;
+    console.log("a=>",a); 
+    console.log("b=>",b); 
+}
+
+foo({})
+*/
+
+// I want to write the same function now but in a  little different way
+// so we have got two varaibles declared a and b both have the default values
+
+/*
+function foo({a,b}={a:"",b:10}){
+    
+    console.log("a=>",a); 
+    console.log("b=>",b); 
+}
+
+foo()
+//foo({}) // Error-> ('{}' is not assignable to parameter of type '{a:string,b:number}'); 
+*/
+
+
+
+
+
+// Default and Opitional
+
+function foo({a,b=0}={a:""}){ // Here b is optional but a is required
+    
+    console.log("a=>",a); 
+    console.log("b=>",b); 
+}
+
+foo();
+foo({a:"abc"})
+// foo({}) Error
+
